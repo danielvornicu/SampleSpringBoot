4425dd45c1ff7255f11a4c58f5dc2685738363e5
danielvornicu <vdany2003@yahoo.com>
2019-01-29 11:40:46 +0100

    commit modified

--- src/main/java/tech/dev/MvcConfiguration.java
+++ src/main/java/tech/dev/MvcConfiguration.java
@@ -1,5 +1,15 @@
 package tech.dev;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
 /**
  * Description de la classe
  * <p>
@@ -9,5 +19,24 @@ package tech.dev;
  * @version 1.0 $Revision$ $Date$
  */
 
-public class MvcConfiguration {
+@Configuration
+@EnableWebMvc
+public class MvcConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public ViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver resolver  = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/pages/");
+		resolver.setSuffix(".jsp");
+		return resolver ;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages/common");
+		source.setDefaultEncoding("UTF-8");
+		return source;
+	}
 }
