7d4b5020258af75e408464ad02363f32b4e47abe
danielvornicu <vdany2003@yahoo.com>
2019-01-29 11:37:32 +0100

    Commit initial

---
+++ src/main/java/tech/dev/MvcConfiguration.java
@@ -0,0 +1,13 @@
+package tech.dev;
+
+/**
+ * Description de la classe
+ * <p>
+ * Date: 28/01/2019
+ *
+ * @author d.vornicu
+ * @version 1.0 $Revision$ $Date$
+ */
+
+public class MvcConfiguration {
+}
