bdbad48e2f9df4625d9a40341b7d544bd13b5493
danielvornicu <vdany2003@yahoo.com>
2023-03-13 20:09:21 +0100

    Postgres properties for Render

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,18 +3,17 @@
 -- CREATE TABLE CLIENT(ID integer auto_increment primary key, PRENOM VARCHAR(255), NOM VARCHAR(255), ADRESSE_ID integer);
 -- CREATE TABLE COMMANDE(ID integer auto_increment primary key, NOMBRE_PRODUITS integer, MONTANT integer, DATE_COMMANDE DATE, CLIENT_ID integer);
 
-/*
-CREATE TABLE users(
-	username varchar_ignorecase(50) not null primary key,
-	password varchar_ignorecase(50) not null,
-	enabled boolean not null
-);
+-- CREATE TABLE users(
+	-- username varchar_ignorecase(50) not null primary key,
+	-- password varchar_ignorecase(50) not null,
+	-- enabled boolean not null
+-- );
 
-CREATE TABLE authorities (
-	username varchar_ignorecase(50) not null,
-	authority varchar_ignorecase(50) not null,
-	constraint fk_authorities_users foreign key(username) references users(username)
-); */
+-- CREATE TABLE authorities (
+	-- username varchar_ignorecase(50) not null,
+	-- authority varchar_ignorecase(50) not null,
+	-- constraint fk_authorities_users foreign key(username) references users(username)
+-- ); 
 
 
 --Postgres SQL Database
